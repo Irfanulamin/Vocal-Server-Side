aee628540a3509a93a4e6502fbbc8ad64e14cfb2
unknown <irfanulamin07@gmail.comm>
2023-06-12 01:33:39 +0600

    role and users route has been added

--- index.js
+++ index.js
@@ -3,5 +3,127 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+// vocalAdmin;
+// D6MQmYH2jKntjyCb
+
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://vocalAdmin:D6MQmYH2jKntjyCb@cluster0.miqdtcr.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const classCollection = client.db("VocalStudioDB").collection("classes");
+    const selectedCollection = client
+      .db("VocalStudioDB")
+      .collection("selected");
+    const usersCollection = client.db("VocalStudioDB").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user?.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.get("/selectedItems", async (req, res) => {
+      const email = req.query.userEmail;
+      console.log(email);
+      const query = { userEmail: email };
+      const result = await selectedCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/selectedItems", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await selectedCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/selectedItems/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Vocal is signing");
+});
+
+app.listen(port, () => {
+  console.log(`Vocal is signing on port ${port}`);
+});
